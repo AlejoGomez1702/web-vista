eadd8256a9a3d844de7049608f91fc53c5f9ca35
Yeferson Castro <yefersoncastrogiraldo@gmail.com>
2024-06-07 21:14:23 -0500

    Section video

--- src/app/index/components/banner/banner.component.ts
+++ src/app/index/components/banner/banner.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-  
 
+  currentIndex = 0;
+  nextIndex = 0;
+  texts = ['Texto 1', 'Texto 2', 'Texto 3'];
+  delayTime = 360;
+
+  constructor() {}
+
+  async nextText() {
+    this.nextIndex = (this.currentIndex < this.texts.length - 1? this.currentIndex + 1 : 0);
+    await new Promise(resolve => setTimeout(resolve, this.delayTime));
+    this.currentIndex = this.nextIndex;
+  }
+
+  async prevText() {
+    this.nextIndex = (this.currentIndex > 0? this.currentIndex - 1 : this.texts.length - 1);
+    await new Promise(resolve => setTimeout(resolve, this.delayTime));
+    this.currentIndex = this.nextIndex;
+  }
   public canShowVideo: boolean = false;
 
   ngOnInit() {
