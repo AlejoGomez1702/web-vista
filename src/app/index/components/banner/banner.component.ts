04d5e5646489edba69bc0c7c197e884d13dba4ee
Yeferson Castro <yefersoncastrogiraldo@gmail.com>
2024-06-29 09:36:26 -0500

    Retouches of the video section

--- src/app/index/components/banner/banner.component.ts
+++ src/app/index/components/banner/banner.component.ts
@@ -6,22 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-
   currentIndex = 0;
   nextIndex = 0;
-  texts = ['Texto 1', 'Texto 2', 'Texto 3'];
+  texts = [
+    {
+      header: 'En Servicio Somos: Rápidos, Atentos Y Profesionales',
+      subheader: '#lavidamasfacil',
+      styles: {
+        header: {
+          'font-size': '80px',
+          'font-weight': '550',
+          'color': 'white',
+          'text-align': 'center'
+        },
+        subheader: {
+          'font-size': '30px',
+          'font-weight': 'normal',
+          'color': '#ffb600',
+          'text-align': 'center'
+        }
+      }
+    },
+    {
+      header: 'Nuestra Imagen Refleja Solidez, Modernismo Y Foco',
+      subheader: '',
+      styles: {
+        header: {
+          'font-size': '80px',
+          'font-weight': '550',
+          'color': 'white',
+          'text-align': 'center'
+        },
+        subheader: {
+          'font-size': '18px',
+          'font-weight': 'normal',
+          'color': 'black',
+          'text-align': 'center'
+        }
+      }
+    },
+    {
+      header: 'El Tiempo Es Todo',
+      subheader: '',
+      styles: {
+        header: {
+          'font-size': '80px',
+          'font-weight': '550',
+          'color': 'white',
+          'text-align': 'center'
+        },
+        subheader: {
+          'font-size': '18px',
+          'font-weight': 'normal',
+          'color': 'brown',
+          'text-align': 'center'
+        }
+      }
+    }
+  ];
   delayTime = 360;
 
   constructor() {}
 
   async nextText() {
-    this.nextIndex = (this.currentIndex < this.texts.length - 1? this.currentIndex + 1 : 0);
+    this.nextIndex = (this.currentIndex < this.texts.length - 1 ? this.currentIndex + 1 : 0);
     await new Promise(resolve => setTimeout(resolve, this.delayTime));
     this.currentIndex = this.nextIndex;
   }
 
   async prevText() {
-    this.nextIndex = (this.currentIndex > 0? this.currentIndex - 1 : this.texts.length - 1);
+    this.nextIndex = (this.currentIndex > 0 ? this.currentIndex - 1 : this.texts.length - 1);
     await new Promise(resolve => setTimeout(resolve, this.delayTime));
     this.currentIndex = this.nextIndex;
   }
@@ -32,6 +86,4 @@ export class BannerComponent implements OnInit {
       this.canShowVideo = true;
     }, 5000);
   }
-
-
 }
