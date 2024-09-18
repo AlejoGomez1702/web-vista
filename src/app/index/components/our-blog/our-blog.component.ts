8dda8ce4c5571a43aae81926e83267f1c178320b
Yeferson Castro <yefersoncastrogiraldo@gmail.com>
2024-09-17 20:30:12 -0500

    our blog a quality control

--- src/app/index/components/our-blog/our-blog.component.ts
+++ src/app/index/components/our-blog/our-blog.component.ts
@@ -2,9 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-our-blog',
-  templateUrl: './our-blog.component.html'
+  templateUrl: './our-blog.component.html',
+  styleUrls: ['./our-blog.component.scss']
 })
 export class OurBlogComponent {
+  ngAfterViewInit(): void {
 
+    const carouselContainer = document.getElementById('carouselContainer') as HTMLElement;
+    const prevButton = document.getElementById('carouselPrev') as HTMLElement;
+    const nextButton = document.getElementById('carouselNext') as HTMLElement;
+
+
+    const scrollStep = 240;
+
+
+    const scrollPrev = () => {
+      carouselContainer.scrollBy({ left: -scrollStep, behavior: 'smooth' });
+    };
+
+
+    const scrollNext = () => {
+      carouselContainer.scrollBy({ left: scrollStep, behavior: 'smooth' });
+    };
+
+
+    prevButton?.addEventListener('click', scrollPrev);
+    nextButton?.addEventListener('click', scrollNext);
+  }
 
 }
